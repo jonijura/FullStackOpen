6ab5a80bd3d007b04c6e7560962a161f1e807de3
jonijura <joona.n.j.raty@jyu.fi>
2022-07-01 17:14:57 +0300

    2d

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,8 +1,27 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import contactServices from './services/contacts'
 
+const Person = ({ ids, persons, setPersons }) => {
+  const person = persons.filter(p => p.id === ids)[0]//TODO handle exceptions
+  const poista = () => {
+    if (!window.confirm(`delete contact ${person.name}?`)) return;
+    console.log('poistetaan', person.name)
+    contactServices.remove(person.id).then(response => {
+      setPersons(
+        persons.filter(p => p.id !== ids))//handle exceptions
+    })
+  }
+  return (
+    <tr>
+      <td>{person.name}</td>
+      <td>{person.number}</td>
+      <td><button onClick={poista}>delete</button></td>
+    </tr>
+  )
+}
 
-const Persons = ({ persons, match }) => {
+const Persons = ({ persons, setPersons, match }) => {
   const personsToShow = persons.filter(p =>
     p.name.toLowerCase().includes(match.toLowerCase())
   )
@@ -10,32 +29,42 @@ const Persons = ({ persons, match }) => {
     <table>
       <tbody>
         {personsToShow.map(person =>
-          <tr key={person.name}>
-            <td>{person.name}</td>
-            <td>{person.number}</td>
-          </tr>
+          <Person key={person.id} ids={person.id} persons={persons} setPersons={setPersons} />
         )}
       </tbody>
     </table>
   )
 }
 
-const NewContact = ({persons, setPersons}) => {
+const NewContact = ({ persons, setPersons }) => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    if (persons.filter(d => d.name === newName).length > 0) {
-      alert(`${newName} is already added to phonebook`)
+    const sameName = persons.filter(d => d.name === newName);
+    if (sameName.length > 0) {
+      if (!window.confirm(`
+      ${newName} has already been added, update number?`)) return;
+      else {
+        const updatedEntry = { ...sameName[0], number: newNumber }
+        contactServices.replace(sameName[0].id, updatedEntry)
+          .then(response => {
+            setPersons(
+              persons.map(p => p.id === sameName[0].id ? updatedEntry : p))
+          })
+      }
     }
     else {
       const newEntry = {
         name: newName,
-        number: newNumber,
-        visible: true
+        number: newNumber
       }
-      setPersons(persons.concat(newEntry))
+      contactServices.create(newEntry)
+        .then(response => {
+          console.log(response)
+          setPersons(persons.concat(response))
+        })
     }
   }
 
@@ -46,29 +75,30 @@ const NewContact = ({persons, setPersons}) => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-  return(
-  <form onSubmit={addName}>
-    <div>
-      name: <input value={newName}
-        onChange={handleNameChange} />
-    </div>
-    <div>
-      number: <input value={newNumber}
-        onChange={handleNumberChange} />
-    </div>
-    <div>
-      <button type="submit">add</button>
-    </div>
-  </form>)
+  return (
+    <form onSubmit={addName}>
+      <div>
+        name: <input value={newName}
+          onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber}
+          onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>)
 }
 
-const SetSearch = ({newSearch, handleSearchChange}) => {
-  return(
-  <div>
-  search: <input value={newSearch}
-    onChange={handleSearchChange} />
-</div>
-)}
+const SetSearch = ({ newSearch, handleSearchChange }) => {
+  return (
+    <div>
+      search: <input value={newSearch}
+        onChange={handleSearchChange} />
+    </div>
+  )
+}
 
 const App = () => {
 
@@ -76,10 +106,9 @@ const App = () => {
   const [newSearch, setNewSearch] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons').then(response => {
-        setPersons(response.data)
-      })
+    contactServices.getAll().then(response => {
+      setPersons(response)
+    })
   }, [])
 
   const handleSearchChange = (event) => {
@@ -90,10 +119,10 @@ const App = () => {
     <div>
       <h1>Phonebook</h1>
       <h2>Add a new contact</h2>
-      <NewContact persons={persons} setPersons={setPersons}/>
+      <NewContact persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      <SetSearch newSearch={newSearch} handleSearchChange={handleSearchChange}/>
-      <Persons persons={persons} match={newSearch} />
+      <SetSearch newSearch={newSearch} handleSearchChange={handleSearchChange} />
+      <Persons persons={persons} setPersons={setPersons} match={newSearch} />
     </div>
   )
 
